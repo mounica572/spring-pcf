f782085ef1df441e4c1aee03794410448e447941
Pallavi Chamarthy <pallavi.chamarthy@gmail.com>
2019-08-29 09:39:23 -0700

    React App.js file changes

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state={
+      noteText: '',
+    }
+  }
+
+  updateNoteText(noteText){
+    this.setState({ noteText: noteText.target.value })  
+  }
+
+  render() { 
+    return  ( 
+      <div className="container">
+    
+        <div className="header"> Message Board App!! </div>
+      
+  
+        <input type="text" 
+         ref={((input) => {this.textInput = input})} 
+         className="textInput"
+         value={this.state.noteText}
+         onChangeText={noteText => this.updateNoteText(noteText)}     
+        />
+        
+        <div className="btn"> submit </div>
+      </div>
+    );
+  }
+
+  }
+
 
 export default App;
