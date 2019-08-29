019cb9d87c7086bae015a46247a5e8d074a0dabe
Clayton Gibb <clayton.gibb@slalom.com>
2019-08-29 09:51:53 -0700

    adding message window component to react app

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -1,39 +1,39 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import MessageWindow from "./components/MessageWindow";
 
-class App extends Component{
-
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
-    this.state={
-      noteText: '',
-    }
+    this.state = {
+      noteText: ""
+    };
   }
 
-  updateNoteText(noteText){
-    this.setState({ noteText: noteText.target.value })  
+  updateNoteText(noteText) {
+    this.setState({ noteText: noteText.target.value });
   }
 
-  render() { 
-    return  ( 
-      <div className="container">
-    
-        <div className="header"> Message Board App!! </div>
-      
-  
-        <input type="text" 
-         ref={((input) => {this.textInput = input})} 
-         className="textInput"
-         value={this.state.noteText}
-         onChangeText={noteText => this.updateNoteText(noteText)}     
-        />
-        
-        <div className="btn"> submit </div>
-      </div>
+  render() {
+    return (
+      <React.Fragment>
+        <div className="container">
+          <div className="header"> Message Board App!! </div>
+          <input
+            type="text"
+            ref={input => {
+              this.textInput = input;
+            }}
+            className="textInput"
+            value={this.state.noteText}
+            onChangeText={noteText => this.updateNoteText(noteText)}
+          />
+          <div className="btn"> submit </div>
+        </div>
+        <MessageWindow></MessageWindow>
+      </React.Fragment>
     );
   }
-
-  }
-
+}
 
 export default App;
