3c4eb2ae835b91574bad285f4694bd6b423d1a4e
Clayton Gibb <clayton.gibb@slalom.com>
2019-08-29 13:43:59 -0700

    undo last change of build script

--- ci/build.sh
+++ ci/build.sh
@@ -4,10 +4,6 @@ set -e -x
 
 cd source-code
   ./mvnw clean package
-  cd react-src
-  ./ npm install
-  ./npm run build
-  cp build ../src/main/resources/static
 cd ..
 
 cp source-code/target/helloapi-0.0.1-SNAPSHOT.jar  build-output/.
