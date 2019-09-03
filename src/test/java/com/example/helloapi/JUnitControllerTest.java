8338b84180a227c68165992a3ac6c28a20fa1a6d
Clayton Gibb <clayton.gibb@slalom.com>
2019-09-03 11:34:56 -0700

    Adding sample endpoint test

---
+++ src/test/java/com/example/helloapi/JUnitControllerTest.java
@@ -0,0 +1,16 @@
+
+package com.example.helloapi;
+
+import com.example.helloapi.controller.MessageController;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class JUnitControllerTest {
+
+    @Test
+    public void MessagesControllerTest() {
+        MessageController messageController = new MessageController();
+        String result = messageController.getHi();
+        assertEquals(result, "Hi There dude!");
+    }
+}
